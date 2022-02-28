c86baa5a40ae5fd6074590341c87675a830b3b57
Victor Castro <vcastro2395@gmail.com>
2022-02-28 15:17:06 -0300

    cors config

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 import express from 'express';
+import cors from 'cors'
 
 
 const app = express()
 const port = process.env.PORT || 3000;
+app.use(cors())
 
 app.get('/', function(req, res) {
     res.json({mensagem: "Olá mundo"})
